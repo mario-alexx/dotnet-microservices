36ea4ff78e6385742d6785005360c6536a1bc7bd
mario-alexx <marioatoroh@gmail.com>
2024-07-31 00:34:00 -0300

    adding command repository interface and implementation

---
+++ CommandsService/Data/CommandRepo.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using CommandsService.Models;
+
+namespace CommandsService.Data 
+{
+    /// <summary>
+    /// Provides implementation for the <see cref="ICommandService"/> to manage commands and platforms.
+    /// </summary>
+    public class CommandRepo : ICommandService
+    {
+        private readonly AppDbContext _context;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CommandRepo"/> class with the specified database context.
+        /// </summary>
+        /// <param name="context">The database context to be used by this repository.</param>
+        public CommandRepo(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        /// <inheritdoc/>
+        public void CreateCommand(int platformId, Command command)
+        {
+            if(command is null) 
+            {
+                throw new ArgumentNullException(nameof(command));
+            }
+            command.PlatformId = platformId;
+            _context.Commands.Add(command);
+        }
+
+        /// <inheritdoc/>
+        public void CreatePlatform(Platform plat)
+        {
+            if(plat is null) 
+            {
+                throw new ArgumentNullException(nameof(plat));
+            }
+
+            _context.Platforms.Add(plat);
+        }
+
+        /// <inheritdoc/>
+        public IEnumerable<Platform> GetAllPlatforms()
+        {
+            return _context.Platforms.ToList();
+        }
+
+        /// <inheritdoc/>
+        public Command GetCommand(int platformId, int commandId)
+        {
+            return _context.Commands
+                .Where(c => c.PlatformId == platformId && c.Id == commandId)
+                .FirstOrDefault();
+        }
+
+        /// <inheritdoc/>
+        public IEnumerable<Command> GetCommandsForPlatform(int platformId)
+        {
+            return _context.Commands
+                .Where(c => c.PlatformId == platformId)
+                .OrderBy(c => c.Platform.Name);
+        }
+
+        /// <inheritdoc/>
+        public bool PlatformExist(int PlatformId)
+        {
+            return _context.Platforms.Any(p => p.Id == PlatformId);
+        }
+
+        /// <inheritdoc/>
+        public bool SaveChanges()
+        {
+            return _context.SaveChanges() >= 0;
+        }
+    }
+}
---
+++ CommandsService/Data/ICommandService.cs
@@ -0,0 +1,62 @@
+using System.Collections.Generic;
+using CommandsService.Models;
+
+namespace CommandsService.Data 
+{
+    /// <summary>
+    /// Defines the contract for a service that manages commands and platforms.
+    /// </summary>
+    public interface ICommandService
+    {
+        /// <summary>
+        /// Persists any changes made to the data store.
+        /// </summary>
+        /// <returns>true if changes were saved successfully; otherwise, false.</returns>
+        bool SaveChanges();
+
+        #region Platforms
+        /// <summary>
+        /// Retrieves all platforms.
+        /// </summary>
+        /// <returns>An enumerable collection of platforms.</returns>
+        IEnumerable<Platform> GetAllPlatforms();
+
+        /// <summary>
+        /// Creates a new platform.
+        /// </summary>
+        /// <param name="plat">The platform to create.</param>
+        void CreatePlatform(Platform plat);
+
+        /// <summary>
+        /// Checks if a platform exists.
+        /// </summary>
+        /// <param name="PlatformId">The ID of the platform to check.</param>
+        /// <returns>true if the platform exists; otherwise, false.</returns>
+        bool PlatformExist(int PlatformId);
+        #endregion
+
+        #region Commands
+        /// <summary>
+        /// Retrieves all commands for a specific platform.
+        /// </summary>
+        /// <param name="platformId">The ID of the platform whose commands are to be retrieved.</param>
+        /// <returns>An enumerable collection of commands for the specified platform.</returns>
+        IEnumerable<Command> GetCommandsForPlatform(int platformId);
+
+        /// <summary>
+        /// Retrieves a specific command for a specific platform.
+        /// </summary>
+        /// <param name="platformId">The ID of the platform whose command is to be retrieved.</param>
+        /// <param name="commandId">The ID of the command to retrieve.</param>
+        /// <returns>The command with the specified ID for the specified platform.</returns>
+        Command GetCommand(int platformId, int commandId);
+
+        /// <summary>
+        /// Creates a new command for a specific platform.
+        /// </summary>
+        /// <param name="platformId">The ID of the platform to which the command belongs.</param>
+        /// <param name="command">The command to create.</param>
+        void CreateCommand(int platformId, Command command);
+        #endregion
+    }
+}
