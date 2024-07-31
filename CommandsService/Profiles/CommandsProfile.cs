774fa9b8762dea475e1b862be33bfcb314eb918a
mario-alexx <marioatoroh@gmail.com>
2024-07-31 18:18:34 -0300

    adding automapper profile commands

---
+++ CommandsService/Profiles/CommandsProfile.cs
@@ -0,0 +1,23 @@
+using AutoMapper;
+using CommandsService.Dtos;
+using CommandsService.Models;
+
+namespace CommandsService.Profiles 
+{
+    /// <summary>
+    /// Defines AutoMapper profiles for mapping between command and platform entities and their respective DTOs.
+    /// </summary>
+    public class CommandsProfile : Profile   
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CommandsProfile"/> class.
+        /// </summary>
+        public CommandsProfile()
+        {
+            // Source -> Target
+            CreateMap<Platform, PlatformReadDto>();
+            CreateMap<CommandCreateDto, Command>();
+            CreateMap<Command, CommandReadDto>();
+        }
+    }
+}
