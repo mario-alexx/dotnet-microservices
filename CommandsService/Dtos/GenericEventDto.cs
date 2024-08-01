50dd33138972453a9085a682738d06892303e9e5
mario-alexx <marioatoroh@gmail.com>
2024-08-01 11:52:50 -0300

    adding platPublishedDto class

---
+++ CommandsService/Dtos/GenericEventDto.cs
@@ -0,0 +1,14 @@
+namespace CommandsService.Dtos 
+{
+    /// <summary>
+    /// Data Transfer Object (DTO) for generic events.
+    /// </summary>
+    public class GenericEventDto
+    {
+        /// <summary>
+        /// Gets or sets the event type.
+        /// </summary>
+        /// <value>The event type.</value>
+        public string Event { get; set; }
+    }
+}
