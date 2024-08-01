fd342a661d79e108e349380e68cb37a39c72386d
mario-alexx <marioatoroh@gmail.com>
2024-08-01 11:53:50 -0300

    adding gerericEventDto class

---
+++ CommandsService/Dtos/PlatformPublishedDto.cs
@@ -0,0 +1,26 @@
+namespace CommandsService.Dtos 
+{
+    /// <summary>
+    /// Data Transfer Object (DTO) for published platform events.
+    /// </summary>
+    public class PlatformPublishedDto
+    {
+        /// <summary>
+        /// Gets or sets the unique identifier for the platform.
+        /// </summary>
+        /// <value>The unique identifier for the platform.</value>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the platform.
+        /// </summary>
+        /// <value>The name of the platform.</value>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the event type.
+        /// </summary>
+        /// <value>The event type.</value>
+        public string Event { get; set; }
+    }
+}
