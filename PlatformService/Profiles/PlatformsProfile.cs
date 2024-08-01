2537120a255d3f9e03533111cd135a6921e23f66
mario-alexx <marioatoroh@gmail.com>
2024-08-01 11:48:08 -0300

    updating ctor add map platReadD to platPublisdhD

--- PlatformService/Profiles/PlatformsProfile.cs
+++ PlatformService/Profiles/PlatformsProfile.cs
@@ -4,13 +4,20 @@ using PlatformService.Models;
 
 namespace PlatformService.Profiles 
 {
+    /// <summary>
+    /// Defines AutoMapper profiles for mapping between platform entities and their respective DTOs.
+    /// </summary>
     public class PlatformsProfile : Profile
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PlatformsProfile"/> class.
+        /// </summary>
         public PlatformsProfile()
         {
             // Source -> Target
             CreateMap<Platform, PlatformReadDto>();
             CreateMap<PlatformCreateDto, Platform>();
+            CreateMap<PlatformReadDto, PlatformPublishedDto>();
         }
     }
 }
